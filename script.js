fa699963803fa27bbb86bbb2c656412346c27857
Мария <maria@iMac-Maria.local>
2024-01-15 20:07:37 +0300

    check

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let container = document.querySelector('.container');
 const input = document.querySelector(".input__name");
 const select = document.querySelector(".choose-one");
 const reset = document.querySelector("h1");
-
+const like = document.querySelector(".like");
 
 const createCard = (obj) => {
   const card = document.createElement('article');
@@ -15,14 +15,33 @@ const createCard = (obj) => {
   <p class="gender">Gender: ${obj.gender}</p>
   <p class="house">House: ${obj.house}</p>
   <p class="wand-core">Wand core: ${obj.wand.core}</p>
-  <p class="alive">Alive: ${obj.alive === true ? 'yes' : 'no'}</p>`;
-  return card
+  <p class="alive">Alive: ${obj.alive === true ? "yes" : "no"}</p>
+   
+   <label for="checkbox">    
+<input class="checkbox" id="checkbox" type="checkbox">
+ <span class="circle">  
+      <svg class= "like"
+          width="800px"
+          height="800px"
+          viewBox="0 0 1024 1024"
+         
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M725.333333 192c-89.6 0-168.533333 44.8-213.333333 115.2C467.2 236.8 388.266667 192 298.666667 192 157.866667 192 42.666667 307.2 42.666667 448c0 253.866667 469.333333 512 469.333333 512s469.333333-256 469.333333-512c0-140.8-115.2-256-256-256z"
+            fill="lightgray"
+          /></svg >
+        </span>
+ </label>`;
+  
+  container.append(card)
 }
 
 // функция  заполняющая карточки при загрузке страницы
 const creator = () => {
   container.innerHTML = '';
-  data.forEach(el => container.append(createCard(el)))
+  data.forEach(el => createCard(el))
 }
 creator()
 
@@ -36,7 +55,7 @@ let creator2 = () => {
       el.name.toLowerCase().includes(input.value.toLowerCase())
       
     ) {
-      container.append(createCard(el))
+      createCard(el)
     }
   })
 }  
@@ -48,12 +67,12 @@ const creator3 = () => {
   data.forEach((el) => {
     if (el.name.toLowerCase().includes(input.value.toLowerCase()) &&
     el.house === select.value) {
-      container.append(createCard(el));
+      createCard(el);
     } else if (
       select.value === "" &&
       el.name.toLowerCase().includes(input.value.toLowerCase())
     ) {
-       container.append(createCard(el));
+       createCard(el);
     }
   });
 }
